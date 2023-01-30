37f7e89b9da29aa2e92cfc266268e5c800d69fbc
hitch909 <evgenij909@yandex.ru>
2023-01-30 18:02:35 +0500

    38 задача готова.

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Задайте массив вещественных чисел. Найдите разницу
+// между максимальным и минимальным элементов массива.
+
+Console.Clear();
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+for (int i = 0; i < size; i++)
+{
+    numbers[i] = new Random().Next(1, 20);
+    Console.Write($" {numbers[i]} ");
+}
+
+Console.WriteLine();
+int max = 0;
+int min = numbers[0];
+int result = 0;
+foreach (var i in numbers)
+{
+    if (i > max) max = i;
+    else
+    {
+        if (i < min) min = i;
+    }
+}
+result = max - min;
+Console.Write($"разница между максимальным и минимальным элементов массива = {result}");
